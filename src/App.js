203a9175f9a0ad1c124904b2eade0c933357887e
Dagim Asnake <dagimasnake1@gmail.com>
2023-04-05 04:46:37 -1200

    file upload and download

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [path, setPath] = useState(null);
+
+  const download = (e) => {
+    e.preventDefault();
+    // console.log(e.target.href);
+    console.log(e.target.name);
+    fetch(e.target.href, {
+      method: "GET",
+      headers: {},
+    })
+      .then((response) => {
+        response.arrayBuffer().then(function (buffer) {
+          const url = window.URL.createObjectURL(new Blob([buffer]));
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", `${e.target.name}`); //or any other extension
+          document.body.appendChild(link);
+          link.click();
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const [Img, setImg] = useState();
+  useEffect(() => {
+    try {
+      axios({
+        url: `http://localhost:5000/img`,
+      }).then((res) => {
+        setImg(res.data.msg);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(path);
+    const formData = new FormData();
+    formData.append("file", path);
+    const addreport = async () => {
+      const response = await fetch("http://localhost:5000/img", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        console.log("something is wrong");
+      }
+
+      const data = await response.json();
+      console.log(data);
+    };
+
+    addreport();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <form onSubmit={submitHandler}>
+        <input type="file" onChange={(e) => setPath(e.target.files[0])} />
+        <button>submit</button>
+      </form>
+      {Img?.map((img) => (
+        // console.log(img)
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={`http://localhost:5000/${img.path}`}
+          name={`${img.name}`}
+          download
+          onClick={(e) => download(e)}
         >
-          Learn React
+          download
         </a>
-      </header>
+      ))}
     </div>
   );
 }
